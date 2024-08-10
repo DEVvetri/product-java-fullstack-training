0558bd65954bb50ba3ada721ece8b46ae85bc715
DEVvetri <vetrivelmpp004@gmail.com>
2024-08-10 12:24:37 +0530

    java

---
+++ javacore/Demo.java
@@ -0,0 +1,11 @@
+package javacore;
+public class Demo {
+    public static void main(String[] args) {
+        int empId;
+        String empName;
+        float empSalary;
+        
+    }
+
+    
+}
